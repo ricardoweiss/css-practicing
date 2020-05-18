850b0c1629dbfc42e441e75079d3b6bd75efc6ea
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-05-18 12:56:21 -0300

    back to top footer btn and landing btn now work with animation

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
-const counter = document.querySelector('.counter');
-const button = document.querySelector('.button');
+const footerBtn = document.querySelector('.back-to-top');
+const landingBtn = document.querySelector('.button');
+const aboutSection = document.getElementById('about-section');
 
-const startClock = () => {
-    setInterval(() => {
+landingBtn.addEventListener('click', smoothScrollDown);
+footerBtn.addEventListener('click', smoothScrollTop);
 
-    counter.innerHTML = "breno"
 
-    }, 1000)
+function smoothScrollDown() {
+    const targetPosition = aboutSection.offsetTop;
+    goToTarget(targetPosition)
+}
 
-};
 
-button.addEventListener('click', startClock);
+function smoothScrollTop() {
+    const targetPosition = 0;
+    goToTarget(targetPosition)
+}
+function goToTarget(targetPosition) {
+    const startPosition = window.pageYOffset;
+    console.log(startPosition);
+    const distance = targetPosition - startPosition;
+    const duration = 750
+    let start = null;
+
+    window.requestAnimationFrame(step)
+
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if (progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+function easeInOutCubic(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t + 2) + b;
+}
