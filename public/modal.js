195561b1d508a2963f48732c3d5dffd904f2d415
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-05-29 13:02:03 -0300

    modal comments

--- public/modal.js
+++ public/modal.js
@@ -17,17 +17,20 @@ function closeModal(modal) {
     modal.style.display = "none";
 }
 
+// Open works modal's event listener
 for (let i = 0; i < worksPhotos.length; i++) {
     worksPhotos[i].addEventListener('click', openModal.bind(null, worksModal[i]))
     console.log('entrou 1');
 }
 
+// Close works modal's button event listener
 for (let i = 0; i < worksCloseButtons.length; i++) {
     worksCloseButtons[i].addEventListener('click', closeModal.bind(null, worksModal[i]))
     console.log('entrou 2');
 
 }
 
+// Close works modal blackdrop event listener
 for (let i = 0; i < worksModal.length; i++) {
     backdrop.addEventListener('click', closeModal.bind(null, worksModal[i]))
     console.log('entrou 3');
@@ -36,19 +39,21 @@ for (let i = 0; i < worksModal.length; i++) {
 
 
 // Certificates modal -->
-
 const certificatesCloseButtons = document.querySelectorAll('.button-close-certificate');
 const certificateModals = document.querySelectorAll('.certificates__modal');
 const certificatePhotos = document.querySelectorAll('.certificates__photo');
 
+// Close certificate modal's backdrop event listener
 for (let i = 0; i < certificateModals.length; i++) {
     backdrop.addEventListener('click', closeModal.bind(null, certificateModals[i]));
 }
 
+// Close certificate modal's button event listener
 for (let i = 0; i < certificatesCloseButtons.length; i++) {
     certificatesCloseButtons[i].addEventListener('click', closeModal.bind(null, certificateModals[i]))
 }
 
+// Open certificate modal's event listener
 for (let i = 0; i < certificatePhotos.length; i++) {
     certificatePhotos[i].addEventListener('click', openModal.bind(null, certificateModals[i]))
 }
